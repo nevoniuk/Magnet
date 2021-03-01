5e6523933c8315937bc4b2c74e05acc7567219fa
nevoniuk <nevoniuk@purdue.edu>
2021-03-01 14:34:57 -0500

    Added Registration page 3/1

---
+++ Magnet/RegistrationViewController.swift
@@ -0,0 +1,64 @@
+//
+//  RegistrationViewController.swift
+//  Magnet
+//
+//  Created by Natalie Evoniuk on 2/28/21.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var emailtext: UITextField!
+    @IBOutlet weak var passwordtext: UITextField!
+    @IBOutlet weak var registerbutton: UIButton!
+    
+    @IBAction func registerclicked(_ sender: Any) {
+        var email = emailtext.text
+        var password = passwordtext.text
+    }
+    @IBOutlet weak var selectsportbttn: UIButton!
+    @IBOutlet weak var tbleview: UITableView!
+    let sportsList = ["Soccer", "Tennis", "BasketBall", "Running"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tbleview.isHidden = true
+        tbleview.delegate = self
+        tbleview.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickedbutton1(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) {
+            self.tbleview.isHidden = false
+        }
+    }
+    func animate(toggle: Bool) {
+        if toggle {
+            UIView.animate(withDuration: 0.3) {
+                self.tbleview.isHidden = false
+            }
+        }
+        else {
+            UIView.animate(withDuration: 0.3) {
+                self.tbleview.isHidden = true
+            }
+        }
+    }
+
+}
+
+extension RegistrationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sportsList.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = sportsList[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectsportbttn.setTitle((sportsList[indexPath.row]), for: .normal)
+        animate(toggle: false)
+    }
+}
