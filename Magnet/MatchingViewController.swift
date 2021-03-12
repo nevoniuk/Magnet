cb3b4234408accf9e603a62c65f6670f278d5612
nevoniuk <nevoniuk@purdue.edu>
2021-03-11 22:13:00 -0500

    another commit

---
+++ Magnet/MatchingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MatchingViewController.swift
+//  Magnet
+//
+//  Created by Natalie Evoniuk on 3/11/21.
+//
+
+import UIKit
+
+class MatchingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //logic for Matching algorithm:
+    //1.Retrieve User key from Login/Registration
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
