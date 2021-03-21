60788cb41e681b0af5e6824d870a432fd48553eb
estebanrichey <richeye@purdue.edu>
2021-03-21 16:32:51 -0700

    Implemented Delete Account Button with user confirmation

--- Magnet/ProfilePageVC.swift
+++ Magnet/ProfilePageVC.swift
@@ -12,38 +12,43 @@ import FirebaseDatabase
 
 class ProfilePageView: UIViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+
+    
     @IBAction func deleteAccountPressed(_sender: UIButton) {
         
-        var confirmDelete = false
+        var confirmDelete = 0
         let alert = UIAlertController(title: "Confirm Account Deletion", message: "Are you sure you want to delete your account? This cannot be undone.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: {_ in NSLog("The \"OK\" alert occured.")}))
-        
-        self.present(alert, animated: true, completion: nil)
-        
-        
-        if confirmDelete {
+        alert.addAction(UIAlertAction(title: NSLocalizedString("DELETE ACCOUNT", comment: "Default action"), style: .destructive, handler: {_ in NSLog("The \"DELETE\" alert occured.")
+            confirmDelete = 1
+            NSLog("%d", confirmDelete)
             
-            let user = Auth.auth().currentUser
-            
-            
-            user?.delete { error in
-                if let error = error {
-                    //an error occured
-                    print("An Error Occured. Deletion of account unsuccesful")
-                } else {
-                    //Account Deleted succesfully
-                    print("Account deleted succesfully")
+            if confirmDelete == 1{
+                let user = Auth.auth().currentUser
+                
+                
+                user?.delete { error in
+                    if let error = error {
+                        //an error occured
+                        print("An Error Occured. Deletion of account unsuccesful")
+                    } else {
+                        //Account Deleted succesfully
+                        print("Account deleted succesfully")
+                    }
+                    
                 }
                 
             }
             
-        }
+        }))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel action"), style: .cancel, handler: {_ in NSLog("The \"CANCEL\" alert occured.")}))
         
+        self.present(alert, animated: true, completion: nil)
     }
     
     
