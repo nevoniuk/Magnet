412fc8ba4647fd42cb1d91afd24931a14c3f131b
estebanrichey <richeye@purdue.edu>
2021-04-18 14:00:13 -0700

    Implemented the chosen photo being attached to the created email

--- Magnet/EmailVC.swift
+++ Magnet/EmailVC.swift
@@ -10,28 +10,41 @@ import MessageUI
 
 
 class EmailVC: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-    
-    
+        
+    let myPickerController = UIImagePickerController()
+    let mailPicker = MFMailComposeViewController()
+
     
     @IBAction func attach(_ sender: Any) {
-        let myPickerController = UIImagePickerController()
+        //SHOW UIIMAGEPICKER
         myPickerController.delegate = self
         myPickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        myPickerController.allowsEditing = true
+        myPickerController.mediaTypes = ["public.image"]
         self.present(myPickerController, animated: true, completion: nil)
+        
+        //END SHOW UIIMAGEPICKER
     }
     @IBOutlet weak var Attach: UIButton!
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        let imageJPEG = image.jpegData(compressionQuality: 1.0)
+        mailPicker.addAttachmentData(imageJPEG!, mimeType: "image/jpeg", fileName: "report_attachment")
+    }
+    
+    
     @IBAction func sendMail(_ sender: Any) {
-        let picker = MFMailComposeViewController()
-        picker.mailComposeDelegate = self
-        picker.setToRecipients(["magnet_app@outlook.com"])
+        mailPicker.mailComposeDelegate = self
+        mailPicker.setToRecipients(["magnet_app@outlook.com"])
         if let subjectText = Subject.text {
-            picker.setSubject(subjectText)
+            mailPicker.setSubject(subjectText)
         }
         
-        picker.setMessageBody(body.text, isHTML: true)
+        mailPicker.setMessageBody(body.text, isHTML: true)
         if MFMailComposeViewController.canSendMail() {
-            present(picker, animated: true, completion: nil)
+            present(mailPicker, animated: true, completion: nil)
         } else {
             let alert = UIAlertController(title: "Email not set up", message: "Email is not set up on this device. Are you using a Simulator rather than real hardware?", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK Action"), style: .cancel, handler: {_ in NSLog("The \"CANCEL\" alert occured.")}))
